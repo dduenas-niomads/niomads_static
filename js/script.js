128cbd854a6b101e71e8621a48b67cc4fe03c799
daniel <dduenas@tumipos.com>
2021-02-18 13:33:28 -0500

    Fix: niomads 18/02/2021 facelift

--- js/script.js
+++ js/script.js
@@ -67,6 +67,37 @@ $(window).on("load", function() {
         }
     });
 
+    if($('#contact-form').length){
+        $('#submit').on("click", function(){
+          var form = '#contact-form';
+          var name = $('#contact-form .name').val();
+          var email = $('#contact-form .email').val();
+          if(name == '' || email == '')
+          {
+            $('#contact-form .response').html('<div class="failed">Porfavor, complete los campos de nombre y correo electrónico.</div>');
+            return false;
+          }
+          $.ajax({
+            url:"sendemail.php",
+            method:"POST",
+            data: $(form).serialize(),
+            beforeSend:function(){
+                $('#contact-form .response').html('<div class="text-info"><img src="images/preloader.gif"> Enviando...</div>');
+            },
+            success:function(data){
+                $('form').trigger("reset");
+                $('#contact-form .response').fadeIn().html(data);
+                setTimeout(function(){
+                    $('#contact-form .response').fadeOut("slow");
+                }, 5000);
+            },
+            error:function(){
+                $('#contact-form .response').fadeIn().html(data);
+            }
+          });
+        });
+      }
+
 });
 
 
